e8654d477b8d7b263e3253c7098a53820d543b6a
Stephano <stephanosalazar18@gmail.com>
2025-12-13 20:17:20 -0400

    Agregar archivos de módulos y definiciones globales para el sistema de gestión hospitalario

---
+++ estructuras.h
@@ -0,0 +1,79 @@
+// Definiciones globales.
+
+#ifndef ESTRUCTURAS_H
+#define ESTRUCTURAS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum NivelUrgencia {
+  CRITICO,
+  URGENTE,
+  MODERADO,
+  LEVE
+};
+
+enum TipoHabitacion {
+  INDIVIDUAL,
+  AISLAMIENTO,
+  CUIDADOS_ESPECIALES,
+  DOBLE,
+};
+
+enum TipoExamen {
+  HEMATOLOGIA,
+  QUIMICA,
+  IMAGENOLOGIA,
+  ORINA,
+  HECES
+};
+
+struct Fecha {
+  int dia;
+  int mes;
+  int anio;
+};
+
+struct Hora {
+  int hora;
+  int minuto;
+};
+
+struct Paciente {
+  
+  // identificacion
+  string cedula;
+  string nombre;
+
+  // Datos Clinicos
+  string motivoConsulta;
+  NivelUrgencia nivelUrgencia;
+
+  // Control administrativo
+  int numLlegada;
+
+};
+
+inline string obtenerNombreUrgencia(NivelUrgencia urgencia) {
+  switch(urgencia) {
+    case CRITICO: return "CRITICO";
+    case URGENTE: return "URGENTE";
+    case MODERADO: return "MODERADO";
+    case LEVE: return "LEVE";
+    default: return "DESCONOCIDO";
+  }
+}
+
+
+// funciones auxiliares
+
+// Sobrecarga simple para comparar fechas
+inline bool fechaEsMenor(Fecha a, Fecha b) {
+  if ( a.anio != b.anio) return a.anio < b.anio;
+  if ( a.mes != b.mes) return a.mes < b.mes;
+  return a.dia < b.dia;
+}
+
+#endif
---
+++ laboratorio.cpp
@@ -0,0 +1,1 @@
+// Módulo 5 (Cola Circular y Pila).
